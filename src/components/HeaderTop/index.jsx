d756b5a9cec9f7544ddc4dfc7d55c77fc92022ba
Kuat <tipoproger@gmail.com>
2026-08-27 10:45:27 +0500

    style: стилизация поиска

--- src/components/HeaderTop/index.jsx
+++ src/components/HeaderTop/index.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/logo.svg'
+import search from '../../assets/search.svg'
 import cls from './HeaderTop.module.css'
 
 const HeaderTop = () => {
@@ -8,6 +9,12 @@ const HeaderTop = () => {
 				<img src={logo} alt='logo' />
 			</button>
 			<button className={cls.catalog}>☰ Каталог </button>
+			<form>
+				{' '}
+				<a href='#'>
+					<img src={search} alt='search' />
+				</a>
+			</form>
 		</div>
 	)
 }
